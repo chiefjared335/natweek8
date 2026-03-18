6975926c9c86ca1584a8c28def0e0b72d35974fe
chiefjared335 <jaredaertsschoolopdrachten@gmail.com>
2026-03-18 11:35:54 +0100

    ui gemaakt

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -14,11 +14,50 @@ namespace natopdrachtweek8
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
-    {
+    
+        public partial class MainWindow : Window
+        {
+            Random random = new Random();
+
+            int getal1;
+            int getal2;
+            string math;
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+            void makeNewMath()
+            {
+                getal1 = random.Next(0, 11);
+                getal2 = random.Next(0, 11);
+
+                int choice = random.Next(1, 5);
+
+                if (choice == 1)
+                {
+                    math = "+";
+                }
+                else if (choice == 2)
+                {
+                    math = "-";
+                }
+                else if (choice == 3)
+                {
+                    math = "*";
+                }
+                else if (choice == 4)
+                {
+                    math = "/";
+                }
+
+                sum.Text = getal1 + " " + math + " " + getal2;
+            }
+
+            private void newMathButton_Click(object sender, RoutedEventArgs e)
+            {
+               makeNewMath();
+            }
+        }
     }
-}
